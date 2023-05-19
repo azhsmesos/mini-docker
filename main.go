ac4b8e053203643c914b2025f5c0fe18b9599171
Unknowf <zzhncut123@163.com>
2023-05-19 23:03:29 +0800

    fix: sh error

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 	cmd := exec.Command("/bin/sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |
-			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER,
 	}
 	cmd.SysProcAttr.Credential = &syscall.Credential{
 		Uid: uint32(1),
