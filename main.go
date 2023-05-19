49a56f366e682a3052fa4c5d0676b1dbe8096e27
Unknowf <zzhncut123@163.com>
2023-05-19 22:55:49 +0800

    feat: add User Namespace

--- main.go
+++ main.go
@@ -12,7 +12,12 @@ func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC |
-			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+			syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER,
+	}
+	cmd.SysProcAttr.Credential = &syscall.Credential{
+		Uid: uint32(1),
+		Gid: uint32(1),
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -20,4 +25,5 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err)
 	}
+	os.Exit(-1)
 }
